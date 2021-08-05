cfe89b96df39fe42a5791fa0ca02e444686cb9b7
antonreed <liveforlfc@gmail.com>
2021-08-05 15:58:18 +0300

    Lesson 3 (Getters, setters)

--- javaLessons/src/main/java/school/lesson3/Employee.java
+++ javaLessons/src/main/java/school/lesson3/Employee.java
@@ -1,12 +1,12 @@
 package school.lesson3;
 
 public class Employee {
-    public String fullName;
-    public String position;
-    public String email;
-    public String phoneNumber;
-    public int salary;
-    public int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
     public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
         this.fullName = fullName;
@@ -17,13 +17,61 @@ public class Employee {
         this.age = age;
     }
 
-    public void info() {
-        System.out.println("Сотрудник: " + fullName);
-        System.out.println("Должность: " + position);
-        System.out.println("E-mail: " + email);
-        System.out.println("Номер телефона: " + phoneNumber);
-        System.out.println("Зарплата: " + salary);
-        System.out.println("Возраст: " + age);
+    public String toString() {
+        return "Сотрудник: " + fullName + "\n"
+                + "Должность: " + position + "\n"
+                + "E-mail: " + email + "\n"
+                + "Номер телефона: " + phoneNumber + "\n"
+                + "Зарплата: " + salary + "\n"
+                + "Возраст: " + age + "\n";
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 
