1f2485aa61547470c317490eef252930f2edcb83
antonreed <liveforlfc@gmail.com>
2021-08-19 17:04:56 +0300

    Lesson 7 (with try-catch)

--- javaLessons/src/main/java/school/lesson7/AppData.java
+++ javaLessons/src/main/java/school/lesson7/AppData.java
@@ -1,17 +1,11 @@
 package school.lesson7;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AppData {
     private String[] header;
     private int[][] data;
-
-    public AppData() {
-
-    }
-
-    public AppData(String[] header, int[][] data) {
-        this.header = header;
-        this.data = data;
-    }
 
     public String[] getHeader() {
         return header;
@@ -25,19 +19,33 @@ public class AppData {
         return data;
     }
 
-    public void setData(int[][] data) {
-        this.data = data;
+    public void setData(ArrayList<int[]> arrayList) {
+        data = new int[arrayList.size()][];
+        arrayList.toArray(data);
     }
 
-    public void setArray(int index, int[] array) {
+    public void addToData(int[] array) {
         try {
-            data[index] = array;
-        } catch (Exception e) {
-            e.printStackTrace();
+            ArrayList<int[]> arrayList = new ArrayList<>(Arrays.asList(data));
+            arrayList.add(array);
+            setData(arrayList);
+        } catch (NullPointerException e) {
+            System.out.println("Не удалось добавить данные.");
         }
     }
 
-    public void info() {
+    public void removeFromData(int index) {
+        try {
+            ArrayList<int[]> arrayList = new ArrayList<>(Arrays.asList(data));
+            arrayList.remove(index);
+            setData(arrayList);
+        } catch (NullPointerException e) {
+            System.out.println("В ArrayList ничего нет.");
+        }
+    }
+
+    public void info() throws NullPointerException {
+
         for (String item : header) {
             System.out.printf("%-10s", item);
         }
@@ -46,10 +54,9 @@ public class AppData {
 
         for (int[] array : data) {
             for (int item : array) {
-                System.out.printf("%-10s", item);
+                System.out.printf("%-10d", item);
             }
             System.out.println();
         }
-        System.out.println();
     }
 }
--- javaLessons/src/main/java/school/lesson7/HomeWorkApp.java
+++ javaLessons/src/main/java/school/lesson7/HomeWorkApp.java
@@ -2,67 +2,94 @@ package school.lesson7;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class HomeWorkApp {
-    private static ArrayList<int[]> arrayList = new ArrayList<>();
-    private static String file = "data.csv";
+    private static final String FILE = "data.csv";
     private static final String SPLIT_BY = ";";
 
+
+    /*
+        Немного заморочился и расставил try-catch
+        и возможность добавлять и сохранять данные,
+        даже если загрузка из файла была неудачной.
+        Можно было всё сделать и красивее, но как мне
+        уже было сказано, всё приходит с опытом.
+        И да, возможно я где-то затупил, где-то ошибся...
+        Но у меня уже глаза болят и мозг болит)
+     */
     public static void main(String[] args) {
         AppData appData = new AppData();
+        try {
+            load(appData);
+        } catch (FileNotFoundException e) {
+            System.out.println("Неудачная загрузка данных: файл для загрузки не найден.");
+            System.out.println("Сформировать массив для добавления данных и последующего сохранения? Да/Нет?");
 
-        load(appData);      //чтение
-        appData.info();
-        appData.setArray(2, new int[]{15, 20, 30});
-        save(appData);      //сохранение
+            Scanner sc = new Scanner(System.in);
+            String answer;
+            while (true) {
+                System.out.println("Да/Нет?");
+                answer = sc.next();
+                if (answer.equalsIgnoreCase("да")) {
+                    appData.setHeader(new String[0]);
+                    appData.setData(new ArrayList<>());
+                    break;
+                }
+                if (answer.equalsIgnoreCase("нет")) {
+                    return;
+                }
+            }
+        }
+
+        appData.addToData(new int[]{351, 6567, 1142, 66362});
+        save(appData);
+        //изменяем данные, чтобы увидеть изменения. Данные сохраняются в тот же файл.
     }
 
-    public static void load(AppData appData) {
+    public static void load(AppData appData) throws FileNotFoundException {
+        ArrayList<int[]> arrayList = new ArrayList();
+        String[] header;
+        BufferedReader reader = new BufferedReader(new FileReader(FILE));
+        String line;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String[] header = reader.readLine().split(SPLIT_BY);
-            String line;
+            header = reader.readLine().split(SPLIT_BY);
             String[] stringArrayFromLine;
             int[] intArrayFromLine;
-
-            int x = 0;      //просто счётчик
             while ((line = reader.readLine()) != null) {
                 stringArrayFromLine = line.split(SPLIT_BY);
                 intArrayFromLine = new int[stringArrayFromLine.length];
                 for (int i = 0; i < intArrayFromLine.length; i++) {
                     try {
-                        //пытаемся преобразовать String[] в int[]
                         intArrayFromLine[i] = Integer.parseInt(stringArrayFromLine[i]);
                     } catch (NumberFormatException e) {
                         if ("".equals(stringArrayFromLine[i])) {
-                            System.out.println("Пустая ячейка [" + x + "][" + i + "] заменена на 0");
+                            System.out.println("Пустая ячейка заменена на 0");
                         } else {
-                            System.out.println("Некорректное значение в ячейке [" + x + "][" + i + "] заменено на 0");
+                            System.out.println("Некорректное значение в ячейке заменено на 0");
                         }
                     }
                 }
                 arrayList.add(intArrayFromLine);
-                x++;
             }
-
-            int[][] data = new int[arrayList.size()][arrayList.size()];
-
-            for (int i = 0; i < data.length; i++) {
-                data[i] = arrayList.get(i);
-            }
-
             appData.setHeader(header);
-            appData.setData(data);
+            appData.setData(arrayList);
         } catch (IOException e) {
+            System.out.println("В процессе загрузки данных произошла ошибка.");
             e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.out.println("Загрузка данных не требуется, т.к. файл пуст.");
+            appData.setHeader(new String[0]);
+            appData.setData(arrayList);
         }
     }
 
     public static void save(AppData appData) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE));
             String[] header = appData.getHeader();
-            int[][] data = appData.getData();
+            ArrayList<int[]> arrayList = new ArrayList<>(Arrays.asList(appData.getData()));
 
             for (int i = 0; i < header.length; i++) {
                 if (i != header.length - 1) {
@@ -72,7 +99,7 @@ public class HomeWorkApp {
                 }
             }
 
-            for (int[] array : data) {
+            for (int[] array : arrayList) {
                 for (int i = 0; i < array.length; i++) {
                     if (i != array.length - 1) {
                         writer.write(array[i] + SPLIT_BY);
@@ -82,7 +109,8 @@ public class HomeWorkApp {
                 }
             }
             writer.close();
-        } catch (IOException e) {
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Сохранить данные не удалось.");
             e.printStackTrace();
         }
     }
